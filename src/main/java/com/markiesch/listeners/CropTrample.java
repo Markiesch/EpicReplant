de325a224841c57a503c4a913278dd07e88458b6
Markiesch <marksmeel@gmail.com>
2022-01-19 18:50:30 +0100

    Added option to add disabled worlds

--- src/main/java/com/markiesch/listeners/CropTrample.java
+++ src/main/java/com/markiesch/listeners/CropTrample.java
@@ -11,16 +11,24 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.Plugin;
 
+import java.util.List;
+
 public class CropTrample implements Listener {
     private final Plugin plugin = EpicReplant.instance;
 
     @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
         if (!event.getAction().equals(Action.PHYSICAL)) return;
+
         Player player = event.getPlayer();
         if (plugin.getConfig().getBoolean("CropTrampling.requirePermission") && !player.hasPermission("epicreplant.trampling")) return;
+
         Block block = event.getClickedBlock();
         if (block == null) return;
+
+        List<String> disabledWorlds = plugin.getConfig().getStringList("CropTrampling.disabledWorlds");
+        if (disabledWorlds.contains(block.getWorld().getName())) return;
+
         if (block.getType() != Material.FARMLAND) return;
         if (plugin.getConfig().getBoolean("CropTrampling.requireBoots")) {
             if (player.getInventory().getBoots() == null) return;
