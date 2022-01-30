9d4109fcba631ab855fead8161f4b24d91fa196c
Markiesch <marksmeel@gmail.com>
2022-01-30 19:51:51 +0100

    Added config option: useDurability

--- src/main/java/com/markiesch/listeners/CropTrample.java
+++ src/main/java/com/markiesch/listeners/CropTrample.java
@@ -14,25 +14,24 @@ import org.bukkit.plugin.Plugin;
 import java.util.List;
 
 public class CropTrample implements Listener {
-    private final Plugin plugin = EpicReplant.instance;
+    private final Plugin plugin = EpicReplant.getInstance();
 
     @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
         if (!event.getAction().equals(Action.PHYSICAL)) return;
 
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("CropTrampling.requirePermission") && !player.hasPermission("epicreplant.trampling")) return;
-
         Block block = event.getClickedBlock();
         if (block == null) return;
+
+        Player player = event.getPlayer();
+        if (plugin.getConfig().getBoolean("CropTrampling.requirePermission") && !player.hasPermission("epicreplant.trampling")) return;
 
         List<String> disabledWorlds = plugin.getConfig().getStringList("CropTrampling.disabledWorlds");
         if (disabledWorlds.contains(block.getWorld().getName())) return;
 
         if (block.getType() != Material.FARMLAND) return;
         if (plugin.getConfig().getBoolean("CropTrampling.requireBoots")) {
-            if (player.getInventory().getBoots() == null) return;
-            if (!player.getInventory().getBoots().getType().equals(Material.LEATHER_BOOTS)) return;
+            if (player.getInventory().getBoots() == null || !player.getInventory().getBoots().getType().equals(Material.LEATHER_BOOTS)) return;
         }
         event.setCancelled(true);
     }
