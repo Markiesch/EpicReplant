1d533a2b18c44fa30c5fc1907c40b06baa4addbc
Markiesch <marksmeel@gmail.com>
2022-01-30 19:50:16 +0100

    Refactored code

--- src/main/java/com/markiesch/EpicReplant.java
+++ src/main/java/com/markiesch/EpicReplant.java
@@ -4,25 +4,26 @@ import com.markiesch.commands.ReplantCommand;
 import com.markiesch.listeners.CropBreak;
 import com.markiesch.listeners.CropTrample;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EpicReplant extends JavaPlugin {
-    public static Plugin instance;
+    private static EpicReplant instance;
+    public static EpicReplant getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
-        // Register commands and Listeners
-        if (getConfig().getBoolean("CropBreak.enabled"))
-            getServer().getPluginManager().registerEvents(new CropBreak(), this);
 
-        if (getConfig().getBoolean("CropTrampling.enabled"))
-            getServer().getPluginManager().registerEvents(new CropTrample(), this);
+        // Register commands and Listeners
+        if (getConfig().getBoolean("CropBreak.enabled")) getServer().getPluginManager().registerEvents(new CropBreak(), this);
+        if (getConfig().getBoolean("CropTrampling.enabled")) getServer().getPluginManager().registerEvents(new CropTrample(), this);
 
         PluginCommand command = getCommand("epicReplant");
         if (command != null) command.setExecutor(new ReplantCommand());
+
         // Send a console message when the plugin is Enabled
         getServer().getConsoleSender().sendMessage("§aEpic Replant is now enabled");
     }
