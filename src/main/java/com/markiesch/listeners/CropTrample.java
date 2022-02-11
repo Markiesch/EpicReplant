4d9fdac765cc9fc3074957d1e462b2c71d58aa7e
Markiesch <marksmeel@gmail.com>
2022-02-11 22:07:16 +0100

    Code cleanup

--- src/main/java/com/markiesch/listeners/CropTrample.java
+++ src/main/java/com/markiesch/listeners/CropTrample.java
@@ -1,9 +1,8 @@
 package com.markiesch.listeners;
 
 import com.markiesch.EpicReplant;
-import com.markiesch.events.CropTrampleEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,62 +15,32 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import java.util.List;
 
 public class CropTrample implements Listener {
+    FileConfiguration config = EpicReplant.getInstance().getConfig();
+
     @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
-        if (!event.getAction().equals(Action.PHYSICAL)) {
-            return;
-        }
+        if (!event.getAction().equals(Action.PHYSICAL) || isDisabledWorld(event.getPlayer().getLocation().getWorld())) return;
 
-        CropTrampleEvent cropTrampleEvent = new CropTrampleEvent(event.getPlayer(), event.getClickedBlock());
-        Bukkit.getPluginManager().callEvent(cropTrampleEvent);
-        if (cropTrampleEvent.isCancelled()) {
-            event.setCancelled(true);
-        }
+        Player player = event.getPlayer();
+        if (config.getBoolean("CropTrampling.requirePermission") && !player.hasPermission("epicreplant.trampling")) return;
+        if (config.getBoolean("CropTrampling.requireBoots") && (player.getInventory().getBoots() == null || !player.getInventory().getBoots().getType().equals(Material.LEATHER_BOOTS))) return;
+
+        event.setCancelled(true);
     }
 
     @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onEntityTrample(EntityInteractEvent event) {
-        if (event.getEntity() instanceof Player) {
-            return;
-        }
-
-        CropTrampleEvent cropTrampleEvent = new CropTrampleEvent(event.getEntity(), event.getBlock());
-        Bukkit.getPluginManager().callEvent(cropTrampleEvent);
-        if (cropTrampleEvent.isCancelled()) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onCropTrample(CropTrampleEvent event) {
-        FileConfiguration config = EpicReplant.getInstance().getConfig();
-        List<String> disabledWorlds = config.getStringList("CropTrampling.disabledWorlds");
-        if (disabledWorlds.contains(event.getBlock().getWorld().getName())) {
-            return;
-        }
-
-        if (event.getBlock().getType() != Material.FARMLAND) {
-            return;
-        }
-
-        if (event.getTrampler() instanceof Player) {
-            Player player = ((Player) event.getTrampler());
-
-            if (config.getBoolean("CropTrampling.requirePermission") && !player.hasPermission("epicreplant.trampling")) {
-                return;
-            }
-
-            if (config.getBoolean("CropTrampling.requireBoots")
-                    && (player.getInventory().getBoots() == null || !player.getInventory().getBoots().getType().equals(Material.LEATHER_BOOTS))) {
-                return;
-            }
-            return;
-        }
+        if (isDisabledWorld(event.getBlock().getWorld()) || !event.getBlock().getType().equals(Material.FARMLAND)) return;
 
         List<String> mobs = config.getStringList("CropTrampling.blacklistedMobs");
-        if (mobs.contains(event.getTrampler().getType().toString()) || mobs.contains("*")) {
-            return;
-        }
+        if (mobs.contains(event.getEntity().getType().toString()) || mobs.contains("*")) return;
         event.setCancelled(true);
+    }
+
+    public boolean isDisabledWorld(World world) {
+        if (world == null) return true;
+
+        List<String> disabledWorlds = config.getStringList("CropTrampling.disabledWorlds");
+        return disabledWorlds.contains(world.toString());
     }
 }
